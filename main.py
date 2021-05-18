99f51e9340f78b38343f4c0263fc6dea4cac403d
Fábio Oliveira <74861781+Fabioo29@users.noreply.github.com>
2021-05-18 16:51:06 +0100

    final version

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from requests import Session, get
+import requests
 from bs4 import BeautifulSoup
 from json import dumps, loads
 import csv
 from tqdm import tqdm
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from argparse import ArgumentParser
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class SSD: 
     def __init__(self, name, password, ano_min, ano_max):
@@ -15,15 +22,74 @@ class SSD:
         self.password = password
         self.ano_min = ano_min
         self.ano_max = ano_max
+        self.pdData = pd.DataFrame({'Data': [], 'Dias': [], 'Origem': []})
 
         self.get_page()
         
     def get_page(self):
+        
         driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+
         driver.get('https://app.seg-social.pt/sso/login')
         driver.find_element_by_id('username').send_keys(str(self.name))
         driver.find_element_by_id('password').send_keys(str(self.password))
         driver.find_element_by_name('submitBtn').click()
+
+        driver.get('https://app.seg-social.pt/ptss/cci/carreiraContributiva/consultar')
+        
+        Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, "ui-commandlink.ui-widget")))
+        ActionChains(driver).move_to_element(Driver_wait).perform()
+
+        driver.find_elements_by_class_name("ui-commandlink.ui-widget")[0].click()
+
+        for ano in tqdm(range(int(self.ano_min), int(self.ano_max)+1)):
+            
+            Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "mainForm:ano_label")))
+            ActionChains(driver).move_to_element(Driver_wait).perform()
+
+            driver.find_element_by_id('mainForm:ano_label').click()
+
+            Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "mainForm:ano_filter")))
+            ActionChains(driver).move_to_element(Driver_wait).perform()
+
+            driver.find_element_by_id('mainForm:ano_filter').send_keys(str(ano))
+            driver.find_element_by_id('mainForm:ano_filter').send_keys(Keys.ENTER)
+
+            Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, 'ui-datatable-tablewrapper')))
+            ActionChains(driver).move_to_element(Driver_wait).perform()
+
+            driver.find_elements_by_class_name("ui-commandlink.ui-widget")[0].click()
+
+            for mes in range(0,12):
+
+                Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="mainForm:mes"]')))
+                ActionChains(driver).move_to_element(Driver_wait).perform()
+
+                driver.find_element_by_xpath('//*[@id="mainForm:mes"]').click()
+
+                try:
+                    driver.find_element_by_xpath('//*[@id="mainForm:mes_' + str(mes) +'"]').click()
+                    
+                    Driver_wait = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, 'ui-datatable-tablewrapper')))
+                    ActionChains(driver).move_to_element(Driver_wait).perform()
+                    
+                    tempData = pd.read_html(driver.page_source)[0].drop(['Unnamed: 2'], axis=1)
+                    tempData['Data'] = "{:02d}/{:4d}".format(mes+1, ano)
+                    self.pdData = self.pdData.append(tempData)
+                except:
+                    break
+
+        #with open("index.html", "w") as file:
+        #    file.write(str(soup))
+
+    def save_data(self, valor):
+        if valor:
+            self.pdData.drop(['Valor'], axis=1)
+        self.pdData.to_csv("output.csv", index=False)
+        pass
+
+
 
 
 if __name__ == "__main__":
@@ -31,8 +97,9 @@ if __name__ == "__main__":
     parser = ArgumentParser(description='Seg-social direta extrato renumerações')
     parser.add_argument('-u','--user', help='Account ID', required=True)
     parser.add_argument('-p','--pw', help='Account password', required=True)
-    parser.add_argument('-d','--data', default="1989/2021", help='1989/2021', required=True)
+    parser.add_argument('-d','--data', default="2000/2021", help='AnoInicio/AnoFim do extrato ex:1989/2021')
+    parser.add_argument('--valor', default=True, help='Eliminar coluna valor? (True/False)')
 
     args = parser.parse_args()
 
-    SSD(args.user, args.pw, args.data.split('/')[0], args.data.split('/')[1]).get_data()
+    SSD(args.user, args.pw, args.data.split('/')[0], args.data.split('/')[1]).save_data(args.valor)
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
-#
+## Extrato de renumerações com dias - Segurança social direta
+
+### Setup
+           
+     install Requirements with:
+        $ pip install -r requirements.txt
+
+### Utilização
+    usage: main.py [-h] -u USER -p PW [-d DATA] [--valor VALOR]
+
+    Seg-social direta extrato renumerações
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    -u USER, --user USER  Account ID
+    -p PW, --pw PW        Account password
+    -d DATA, --data DATA  AnoInicio/AnoFim do extrato ex:1989/2021
+    --valor VALOR         Eliminar coluna valor? (True/False)
